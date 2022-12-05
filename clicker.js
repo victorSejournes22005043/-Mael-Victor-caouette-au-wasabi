153b362336f9871a1abcb8488fba9b5a1cc2619f
leperliermael <leperliermael@gmail.com>
2022-12-05 14:24:56 +0100

    pour Victor le s

---
+++ clicker.js
@@ -0,0 +1,14 @@
+var caouete = 0
+
+function init_clicker() {
+    document.getElementById("clicker").addEventListener("click", plusCaouete)
+    document.getElementById("buttonUp").addEventListener("click", plusCaoueteR)
+}
+
+function plusCaouete() {
+    caouete = caouete + 1
+}
+
+function plusCaoueteR() {
+    
+}
